1b6808c16de687a61f32b6a157c300d5ecb3516b
zyl1314 <zhangyalong@qq.com>
2018-08-22 00:08:05 +0800

    add hrm

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { hot } from 'react-hot-loader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get edit <code>src/App.js</code> and save to reload.
-        </p>
+        hello world!
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
