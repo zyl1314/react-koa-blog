b9baba39e1adc1ec55a23bb474edeec2c6ec7222
zhangyalong <zhangyalong@zhuanzhuan.com>
2018-08-22 22:20:13 +0800

    feat: 结构搭建

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Router, Route} from 'react-router';
 import createHistory from "history/createBrowserHistory";
-import routeConfig from '@router/config'
+// import routeConfig from '@router/config';
+import AuthorizeRoute from '@router/authorizeRoute';
+import DynamicRoute from '@router/dynamicRoute';
 import { hot } from 'react-hot-loader';
+
 const history = createHistory();
 
-const App = () => { 
-  return <Router history={history}>
-    {routeConfig.map((route) => {
-      <Route />
-    })}
+const App = () => (
+  <Router history={history}>
+    <Route path="/login" component={<DynamicRoute component={() => import('@views/user')} />} />
   </Router>
-}
+) 
+
+
 
 export default hot(module)(App);
